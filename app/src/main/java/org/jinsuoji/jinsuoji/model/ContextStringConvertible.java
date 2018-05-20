22de0246447ea320284cf1bd04b92af168fe23ab
gsxab <2072414837@qq.com>
2018-05-20 11:13:40 +0800

    补充了注释.

--- app/src/main/java/org/jinsuoji/jinsuoji/model/ContextStringConvertible.java
+++ app/src/main/java/org/jinsuoji/jinsuoji/model/ContextStringConvertible.java
@@ -2,6 +2,14 @@ package org.jinsuoji.jinsuoji.model;
 
 import android.content.Context;
 
+/**
+ * 通过上下文转成字符串.
+ */
 public interface ContextStringConvertible {
+    /**
+     * 转成字符串.
+     * @param context 上下文对象，用于获取字符串资源
+     * @return 字符串
+     */
     String toContextString(Context context);
 }
